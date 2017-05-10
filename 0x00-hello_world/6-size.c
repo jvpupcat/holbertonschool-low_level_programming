414986b05c8eddae97c635b03aa61361a4b0de43
jvpupcat <vuong.joann@yahoo.com>
2017-05-10 20:46:32 +0000

    corrected char to float - last line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a char: %ld byte(s)\n", sizeof(f));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
