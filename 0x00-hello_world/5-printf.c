bf75489033919a34e15600672964af51ad7bec68
jvpupcat <vuong.joann@yahoo.com>
2017-05-10 20:25:54 +0000

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
