dfdc33a0f3e733314e0f80a36a35bdb97781da17
jvpupcat <vuong.joann@yahoo.com>
2017-05-16 05:35:34 +0000

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - function to return sum of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 upon success
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
