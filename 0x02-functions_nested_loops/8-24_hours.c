8fc091288a75f2a849998b095bb7dbdc4cea5822
jvpupcat <vuong.joann@yahoo.com>
2017-05-16 04:36:41 +0000

    first commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - entry point
+ *
+ * Return: none
+ **/
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 23; x++)
+	{
+		for (y = 0; y <= 59; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
