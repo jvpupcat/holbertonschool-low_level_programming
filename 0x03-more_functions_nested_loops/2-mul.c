8c64d29d0aaedcad3d02b7b9a7126341328b8b9d
jvpupcat <vuong.joann@yahoo.com>
2017-05-18 21:31:56 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 upon success
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
