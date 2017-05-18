1b56f86cfc23dfaf9ec61c4a43f5d82b7efc6b70
jvpupcat <vuong.joann@yahoo.com>
2017-05-18 00:16:44 +0000

    first commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 upon success
+ **/
+int main(void)
+{
+	long int c, next;
+	long int first = 1;
+	long int second = 2;
+	long int even_sum = 2;
+	
+	for (c = 3; c < 33; c++)
+	{
+		if (c <= 1)
+			next = c;
+		else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+			if (next % 2 == 0)
+			{
+				even_sum = even_sum + next;
+			}
+		}
+	}
+	printf("%ld", even_sum);
+	putchar('\n');
+	return (0);
+}
