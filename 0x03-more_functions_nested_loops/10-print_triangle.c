634972d8664fbe6e8a86c99fd4dfb22fd97f54c8
jvpupcat <vuong.joann@yahoo.com>
2017-05-19 02:18:46 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - function that prints triangle
+ * @size: variable that passes through function
+ * Return: none
+ **/
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int z;
+	int n = 1;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size - n; y++)
+		{
+			_putchar(' ');
+		}
+		for (z = 0; z < size - size + n; z++)
+		{
+			_putchar(35);
+		}
+		n++;
+		_putchar('\n');
+	}
+}
