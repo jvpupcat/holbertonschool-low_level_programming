61437a3d505bfa31a8027501b27ee8422d2a7ab6
jvpupcat <vuong.joann@yahoo.com>
2017-05-24 00:36:33 +0000

    first commit

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts_half - function that prints half the string
+ * @str: pointer that points to string of numbers
+ * Return: none
+ **/
+void puts_half(char *str)
+{
+	int x;
+	int y;
+
+	for (x = 0; str[x] != '\0'; x++)
+		;
+	for (y = x / 2; str[y] != '\0'; y++)
+	{
+		_putchar(str[y]);
+	}
+	_putchar('\n');
+}
