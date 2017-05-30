d642784e1bce21e7fc8c714bc718a3c67bdd49d4
jvpupcat <vuong.joann@yahoo.com>
2017-05-30 23:43:17 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer
+ * @c: character
+ * Return: 0 upon success
+ **/
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == c)
+		{
+			return (&s[x]);
+		}
+	}
+	return (0);
+}
