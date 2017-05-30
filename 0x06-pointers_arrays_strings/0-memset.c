9e29b442e2d7ce68471bc1bab206484165966641
jvpupcat <vuong.joann@yahoo.com>
2017-05-30 22:11:04 +0000

    added comments to @n

--- 0x06-pointers_arrays_strings/0-memset.c
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - function that fills memory with a constand byte
  * @s: pointer to a memory block that will be filled
  * @b: the value to be stored
- * @n: 
+ * @n: number of bytes to be set
  * Return: char upon success
  **/
 char *_memset(char *s, char b, unsigned int n)
