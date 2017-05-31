f3208cb8dc5a2c4a97af0d24d71c600070804713
jvpupcat <vuong.joann@yahoo.com>
2017-05-31 18:43:24 +0000

    added comments

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -2,8 +2,9 @@
 
 /**
  * _strstr - function that locates a substring
- *
- *
+ * @haystack: string
+ * @needle: substring
+ * Return: 0 upon success
  **/
 char *_strstr(char *haystack, char *needle)
 {
