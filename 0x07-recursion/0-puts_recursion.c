ad7adbb6ccb41c315dd54f2966d95c2d074675dc
jvpupcat <vuong.joann@yahoo.com>
2017-06-01 21:05:08 +0000

    first commit

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: pointer that points to the string
+ * Return: none
+ **/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
