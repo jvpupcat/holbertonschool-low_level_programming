ae044f072ff533b3643ea3e0a53d582b6e24a46f
jvpupcat <vuong.joann@yahoo.com>
2017-06-01 22:58:36 +0000

    first commit

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: variable that runs through function
+ * Return: 
+ **/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n-1));
+}
