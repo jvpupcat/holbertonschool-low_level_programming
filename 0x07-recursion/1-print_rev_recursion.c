3bed6cc8f36a1e8474a019331999a5f7952400d6
jvpupcat <vuong.joann@yahoo.com>
2017-06-01 21:23:59 +0000

    first commit

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: pointer that points to string
+ * Return: none
+ **/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
