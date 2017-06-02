e8c955a0299fb86f77658e6092d51221babb3c03
jvpupcat <vuong.joann@yahoo.com>
2017-06-02 22:03:40 +0000

    added prime_helper prototype

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -4,6 +4,8 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _helper(int x, int n);
 int _sqrt_recursion(int n);
+int prime_helper(int x, int y);
 int is_prime_number(int n);
 int is_palindrome(char *s);
