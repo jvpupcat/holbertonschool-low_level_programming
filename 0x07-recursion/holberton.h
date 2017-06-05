60a584fbfc668f874eda31da4d7c66d3432ee887
jvpupcat <vuong.joann@yahoo.com>
2017-06-05 01:15:44 +0000

    removed spaces

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -14,5 +13,4 @@ int is_prime_number(int n);
 int length(char *s);
 int _move_through(char *s, int x, int y);
 int is_palindrome(char *s);
-
 #endif
