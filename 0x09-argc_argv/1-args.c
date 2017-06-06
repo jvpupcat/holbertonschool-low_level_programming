8a34db2913a19643e19cd875ef6f6ebdb80f9379
jvpupcat <vuong.joann@yahoo.com>
2017-06-06 00:56:23 +0000

    added comments for argv

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -3,9 +3,10 @@
 /**
  * main - function that counts the number of arguments passed through
  * @argc: argument count
+ * @argv: unused attribute argv
  * Return: 0 upon success
  **/
-int main(int argc,__attribute__((unused)) char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
