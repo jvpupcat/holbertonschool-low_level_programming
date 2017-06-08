f5818a03a361177fa5ecd5302f9f8ed84413b461
jvpupcat <vuong.joann@yahoo.com>
2017-06-08 23:45:25 +0000

    changed 0 to NULL

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
  * malloc_checked - function that allocates memory using malloc
  * @b: unsigned int that passes through function
- * Return: none
+ * Return: a pointer
  **/
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *array;
 
-	array = malloc(sizeof(b));
-	if (array == 0)
+	array = malloc(b);
+	if (array == NULL)
 	{
 		exit(98);
 	}
