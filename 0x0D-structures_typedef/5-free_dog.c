4e7a99e722db9352d0ba39f46b10590f95b86edd
jvpupcat <vuong.joann@yahoo.com>
2017-06-13 05:53:26 +0000

    first commit

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer
+ * Return: none
+ **/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
