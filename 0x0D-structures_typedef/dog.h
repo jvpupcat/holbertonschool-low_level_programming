c15eca823319146ad213b9bbbd1e5a584e42542c
jvpupcat <vuong.joann@yahoo.com>
2017-06-13 01:38:03 +0000

    first commit

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dog - struct
@@ -15,4 +17,9 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/** dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+**/
 #endif
