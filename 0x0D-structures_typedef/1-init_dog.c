11cfacd56f3758f963231abf337e6ad84341e3f7
jvpupcat <vuong.joann@yahoo.com>
2017-06-13 01:43:08 +0000

    added edge cases

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -11,9 +11,13 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		return;
-	}
+	if (name == NULL)
+		return;
+	if (age == 0)
+		return;
+	if (owner == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
