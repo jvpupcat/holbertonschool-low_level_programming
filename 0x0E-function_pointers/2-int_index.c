25dcb6042a6525f05fb308f4c7898660b8a26ae3
jvpupcat <vuong.joann@yahoo.com>
2017-06-14 23:47:00 +0000

    first commit

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: pointer to array
+ * @size: size of array
+ * @cmp: pointer to a function that is used to compare values
+ * Return: 0 upon success
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return (-1);
+	}
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]) != 0)
+		{
+			return (x);
+		}
+	}
+	return (0);
+}
