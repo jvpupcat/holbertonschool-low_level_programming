d78618a009e7d0c6d10c3d083c430ba2003d4150
jvpupcat <vuong.joann@yahoo.com>
2017-06-15 05:05:48 +0000

    added prototypes to header file

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -11,10 +11,14 @@
  **/
 int main(int argc, char *argv[])
 {
+	int x;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
+	x = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", x);
 	return (0);
 }
