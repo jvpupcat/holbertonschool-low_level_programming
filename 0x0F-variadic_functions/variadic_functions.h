f48ecd92a3c4cfdd5e5a5c091867fff9aa8f5c16
jvpupcat <vuong.joann@yahoo.com>
2017-06-17 23:44:29 +0000

    added struct and comments to struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -4,5 +4,15 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * find_func - function that finds a function
+ * @find: find
+ * @function: function
+ * Return: none
+ **/
+typedef struct find_func {
+	char *find;
+	void (*function)();
+} match_t;
 void print_all(const char * const format, ...);
 #endif
