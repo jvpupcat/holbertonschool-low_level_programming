48330ea3cdca848d043f661ed06bbeb7d1c5ccd9
jvpupcat <vuong.joann@yahoo.com>
2017-06-17 01:41:57 +0000

    first commit

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - function that prints strings, followed by newline
+ * @separator: pointer that points to separator of string
+ * @n: string that passes through function
+ * Return: none
+ **/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int x;
+
+	va_start (list, n);
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+	if (n == 0)
+	{
+		printf("nil");
+	}
+	for (x = 0; x < n - 1; x++)
+	{
+		printf("%s%s%s", va_arg(list, char *), separator, va_arg(list, char *));
+	}
+	va_end(list);
+	putchar('\n');
+}
