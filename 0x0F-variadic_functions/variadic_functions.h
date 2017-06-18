a446629a237778ffb37c3eb63c0ca315f94d3641
jvpupcat <vuong.joann@yahoo.com>
2017-06-18 23:46:43 +0000

    moved brace to the next line

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,12 +8,13 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 /**
- * find_func - function that finds a function
+ * struct find_func - function that finds a function
  * @find: find
  * @function: function
  * Return: none
  **/
-typedef struct find_func {
+typedef struct find_func
+{
 	char *find;
 	void (*function)();
 } match_t;
