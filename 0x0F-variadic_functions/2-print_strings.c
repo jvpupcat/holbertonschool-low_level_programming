cc552e13e2a56dc6b8dc7b67628ececf59edd632
jvpupcat <vuong.joann@yahoo.com>
2017-06-18 03:14:32 +0000

    removed extra space

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int x;
 
-	va_start (list, n);
+	va_start(list, n);
 	if (separator == NULL)
 	{
 		separator = "";
