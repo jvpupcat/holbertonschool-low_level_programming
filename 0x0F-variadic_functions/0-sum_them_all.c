baccf41bc429b08d7f8961ee3b8a6a9202097ecc
jvpupcat <vuong.joann@yahoo.com>
2017-06-18 03:09:31 +0000

    removed if statement

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -9,19 +9,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int x;
-	int arg = 0;
+	unsigned int x, arg = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
 	va_start(list, n);
+	for (x = 0; x < n; x++)
 	{
-		for (x = 0; x < n; x++)
-		{
-			arg += va_arg(list, int);
-		}
+		arg += va_arg(list, int);
 	}
 	va_end(list);
 	return (arg);
