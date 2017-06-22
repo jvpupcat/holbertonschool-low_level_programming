f0fdc7f0dcf982177dd0d8746b087ed009785b72
jvpupcat <vuong.joann@yahoo.com>
2017-06-22 23:39:13 +0000

    used my own _strlen function

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - function that counts the length of string
+ * @str: string that passes through function
+ * Return: the length of string
+ **/
+int _strlen(const char *str)
+{
+	int x = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+/**
  * add_node: function that adds a new node at the beginning of a list
  * @head: pointer that points to *head
  * @str: string that passes through function
@@ -11,7 +26,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	unsigned int len = 0;
 
-	len = strlen(str);
+	len = _strlen(str);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
