88c3f8da04b322192795c2d83412e11a4d32f78e
jvpupcat <vuong.joann@yahoo.com>
2017-06-23 04:42:50 +0000

    split printf sentence into 2

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -7,6 +7,7 @@
  **/
 int first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 	return (0);
 }
