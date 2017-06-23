acf809a2d9f0c43c59a394e8e2be8a95369b029a
jvpupcat <vuong.joann@yahoo.com>
2017-06-23 04:39:23 +0000

    first commit

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * first - function that prints before the main function
+ *
+ * Return: 0 upon success
+ **/
+int first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
