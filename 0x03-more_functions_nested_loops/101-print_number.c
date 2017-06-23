f961622f7812832e1cae69520b9c53354fd1a991
jvpupcat <vuong.joann@yahoo.com>
2017-06-23 17:33:29 +0000

    first commit

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: number that passes through function
+ * Return: none
+ **/
+void print_number(int n)
+{
+	int i;
+	int max = 100000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (i = 0; i < 9; i++)
+	{
+		if (n / max != 0)
+		{
+			_putchar(((n / max) % 10) + '0');
+		}
+		max /= 10;
+	}
+}
