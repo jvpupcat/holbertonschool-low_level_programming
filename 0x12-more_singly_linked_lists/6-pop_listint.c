647657063216915238b2b1e2e9f7ebc71b1027a8
jvpupcat <vuong.joann@yahoo.com>
2017-06-27 06:17:05 +0000

    first commit

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node of a linked list
+ * @head: double pointer that points to head
+ * Return: 0 if list is empty
+ **/
+int pop_listint(listint_t **head)
+{
+	listint_t *store_next;
+	int store_num;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	store_num = (*head)->n;
+	store_next = (*head)->next;
+	free (*head);
+	*head = store_next;
+	return (store_num);
+}
