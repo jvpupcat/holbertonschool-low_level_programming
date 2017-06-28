0c7467bea6bf748a1a499b8e21e0ba86ca11793b
jvpupcat <vuong.joann@yahoo.com>
2017-06-28 22:34:24 +0000

    returned *head

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
+		return (*head);
 	}
 	for (x = 0; store_head != NULL; x++)
 	{
