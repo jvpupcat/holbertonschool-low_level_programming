518b1fdcfba0a8965356b28c6b2219dca3141516
jvpupcat <vuong.joann@yahoo.com>
2017-06-29 23:57:15 +0000

    first commit

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * is_it_binary - function that checks to see if string is binary
+ * @b: variable that runs through function
+ * Return: 0 upon success, -1 if not binary
+ */
+int is_it_binary(const char *b)
+{
+	int x;
+
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] != '0' && b[x] != '1')
+			return (-1);
+	}
+	return (0);
+}
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: char that runs through function
+ * Return: the converted number or 0 if error or NULL
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+	int bin_check, x;
+	unsigned int y;
+
+	if (b == NULL)
+		return (0);
+	bin_check = is_it_binary(b);
+	if (bin_check != 1)
+		return (0);
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		y = y << 1;
+		if (b[x] == '1')
+		{
+			y = y + 1;
+		}
+	}
+	return (y);
+}
