13e5a7dd67ac1c61093b55dff3a096c3acd98f1b
jvpupcat <vuong.joann@yahoo.com>
2017-06-30 23:59:45 +0000

    removed trailing whitespace

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask;
 
 	mask = 1 << index;
-	
+
 	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
 	*n = *n | mask;
