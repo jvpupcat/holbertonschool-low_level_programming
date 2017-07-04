d490ef90152fcd2572828afe4d4b249c587a80f3
jvpupcat <vuong.joann@yahoo.com>
2017-07-04 04:45:37 +0000

    fixed betty errors

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - function that appends a text at the end of a file
  * @filename: name of file
- * @text_content - content
+ * @text_content: content
  * Return: 1 upon success and -1 upon failure
  **/
 int append_text_to_file(const char *filename, char *text_content)
@@ -12,19 +12,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	int length;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 	store_open = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (store_open == -1)
 		return (-1);
 	if (text_content == NULL)
-		return -1;
+		return (-1);
 	if (text_content != NULL)
 	{
 		length = 0;
 		while (text_content[length] != '\0')
 			length++;
 		store_write = write(store_open, text_content, length);
-		if(store_write == -1)
+		if (store_write == -1)
 			return (-1);
 	}
 	close(store_open);
