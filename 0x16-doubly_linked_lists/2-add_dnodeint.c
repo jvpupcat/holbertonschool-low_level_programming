be6c97c5baab133214cb51d23a3f635f7ab6bd86
jvpupcat <vuong.joann@yahoo.com>
2017-08-05 02:27:30 +0000

    first commit

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - function that adds a node
+ * @head: double pointer head
+ * @n: variable that passes through function
+ * Return: add_node
+ **/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *add_node;
+
+	add_node = malloc(sizeof(dlistint_t));
+	if (add_node == NULL)
+		return (NULL);
+	add_node->n = n;
+	if (*head == NULL)
+	{
+		*head = add_node;
+		add_node->next = NULL;
+	}
+	else
+	{
+		add_node->next = *head;
+		*head = add_node;
+	}
+	return (add_node);
+}
