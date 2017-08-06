9f9ff7761f419fa138e53445ee5c45e03d949d4d
jvpupcat <vuong.joann@yahoo.com>
2017-08-06 22:15:04 +0000

    broke out function

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * Insert_at_head - function that inserts at head
+ * @n: the variable that passes through function
+ * Return: none
+ **/
+dlistint_t *Insert_at_head(int n)
+{
+	dlistint_t *add_node;
+
+	add_node = malloc(sizeof(dlistint_t));
+	if (add_node == NULL)
+		return (NULL);
+	add_node->n = n;
+	add_node->prev = NULL;
+	add_node->next = NULL;
+	return (add_node);
+}
+
+/**
  * add_dnodeint - function that adds a node
  * @head: double pointer head
  * @n: variable that passes through function
@@ -10,22 +28,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *add_node;
 
-	add_node = malloc(sizeof(dlistint_t));
-	if (add_node == NULL)
-		return (NULL);
-	add_node->n = n;
+	add_node = Insert_at_head(n);
+
 	if (*head == NULL)
 	{
 		*head = add_node;
-		add_node->next = NULL;
-		add_node->prev = *head;
+		return (add_node);
 	}
-	else
-	{
-		add_node->next = (*head)->next;
-		(*head)->prev = add_node->prev;
-		add_node->prev = *head;
-		*head = add_node;
-	}
+	(*head)->prev = add_node;
+	add_node->next = *head;
+	*head = add_node;
 	return (add_node);
 }
