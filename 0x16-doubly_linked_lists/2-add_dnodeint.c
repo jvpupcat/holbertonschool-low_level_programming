d10eda30653149ebd2bc151c35b4a6592de179af
jvpupcat <vuong.joann@yahoo.com>
2017-08-06 00:05:10 +0000

    added if/else statement back

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (add_node == NULL)
 		return (NULL);
 	add_node->n = n;
-	add_node->prev = NULL;
-	add_node->next = *head;
-	*head = add_node;
+	if (*head == NULL)
+	{
+		*head = add_node;
+		add_node->next = NULL;
+		add_node->prev = *head;
+	}
+	else
+	{
+		add_node->next = (*head)->next;
+		add_node->prev = *head;
+		*head = add_node;
+	}
 	return (add_node);
 }
