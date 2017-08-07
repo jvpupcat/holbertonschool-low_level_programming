40dcb96f1162c1ed8e380dbee5639b55b1f24422
jvpupcat <vuong.joann@yahoo.com>
2017-08-07 06:34:41 +0000

    removed whitespaces

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,38 +9,36 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *position, *delete_node;
-        unsigned int i;
+	unsigned int i;
 
-        i = 0;
-        position = *head;
+	i = 0;
+	position = *head;
 
-        if (*head == NULL)
-        {
-                return (-1);
-        }
-        if (index == 0)
-        {
-                delete_node = *head;
-                *head = delete_node->next;
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		delete_node = *head;
+		*head = delete_node->next;
 		delete_node->prev = NULL;
-                free(delete_node);
-                return (1);
-        }
-        while (position != NULL)
-        {
+		free(delete_node);
+		return (1);
+	}
+	while (position != NULL)
+	{
 		if (i == index - 1)
 		{
 			delete_node = position->next;
 			position->next = delete_node->next;
 			delete_node->next->prev = position;
-			free (delete_node);
-			
+			free(delete_node);
 		}
-                position = position->next;
-                i++;
-                if (position == NULL)
-                        return (-1);
-        }
-        return (1);
-
+		position = position->next;
+		i++;
+		if (position == NULL)
+			return (-1);
+	}
+	return (1);
 }
