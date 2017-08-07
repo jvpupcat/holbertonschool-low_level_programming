9f58c36f3d16332121a617e60c4739e0920f4d35
jvpupcat <vuong.joann@yahoo.com>
2017-08-07 02:49:59 +0000

    removed before_idx + 1 = idx

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -60,7 +60,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		before_idx++;
 	}
-	if (temp->next == NULL && before_idx + 1 == idx)
+	if (temp->next == NULL)
 	{
 		temp->next = insert;
 		insert->next = NULL;
