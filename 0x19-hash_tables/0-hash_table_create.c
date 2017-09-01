3c83587b5590f5f3509bca8ab72b4899bf942451
jvpupcat <vuong.joann@yahoo.com>
2017-09-01 19:26:13 +0000

    added 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,6 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable;
 
+	if (size <= 0)
+		return (NULL);
 	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 		return (NULL);
