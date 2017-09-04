79aae9384e7d404d056f2126748c90c304b10de5
jvpupcat <vuong.joann@yahoo.com>
2017-09-04 03:01:08 +0000

    freed more

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -7,13 +7,22 @@
  **/
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *temp;
+	hash_node_t *temp, *node;
+	unsigned long int i = 0;
 
-	temp = ht->array;
-	while (temp != NULL)
+	if (ht == NULL)
+		return;
+	while (i < ht->size)
 	{
-		free(temp->value);
-		free(temp->key);
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			node = temp;
+			free(temp->value);
+			free(temp->key);
+			free(node);
+			temp = temp->next;
+		}
 	}
 	free(ht->array);
 	free(ht);
