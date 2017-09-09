b388b121590133707187bb488c6c7b0011d65958
jvpupcat <vuong.joann@yahoo.com>
2017-09-09 01:53:49 +0000

    added ht->size == 0

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	unsigned long int flag = 0;
 
-	if (ht == NULL)
+	if (ht == NULL || ht->size == 0)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
