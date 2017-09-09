a66bf4ad85e3776fdb2afd46a70092e7ab63fc14
jvpupcat <vuong.joann@yahoo.com>
2017-09-09 00:00:48 +0000

    added another for loop

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -9,25 +9,28 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp;
 	unsigned long int i;
-	int flag = 1;
+	unsigned long int flag = 0;
 
 	if (ht == NULL)
 		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		if (temp != NULL)
+		{
+			flag = i;
+			temp = temp->next;
+		}
+	}
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		temp = ht->array[i];
 		while (temp != NULL)
 		{
-			if (flag == 1)
-			{
-				printf("'%s': '%s', ", temp->key, temp->value);
-				flag = 0;
-			}
-			else
-			{
-				printf(", '%s': '%s'", temp->key, temp->value);
-			}
+			printf("'%s': '%s', ", temp->key, temp->value);
+			if (i != flag)
+				printf(", ");
 			temp = temp->next;
 		}
 	}
