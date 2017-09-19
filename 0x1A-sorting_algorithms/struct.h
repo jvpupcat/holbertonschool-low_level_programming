fdf7f42b0ac7acfb18df70f886c34f96ec706c1b
jvpupcat <vuong.joann@yahoo.com>
2017-09-19 21:13:07 +0000

    added prev

--- 0x1A-sorting_algorithms/struct.h
+++ 0x1A-sorting_algorithms/struct.h
@@ -6,6 +6,7 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
+ * @prev: pointer to the prev element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
