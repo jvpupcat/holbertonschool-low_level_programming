b55002a373a2a1ccc82ed4b63a0b279f9e94ebd8
jvpupcat <vuong.joann@yahoo.com>
2017-09-19 21:14:10 +0000

    added prev

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -7,6 +7,7 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
+ * @prev: Pointer to the next element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
@@ -26,7 +27,7 @@ void print_list(const listint_t *list);
 /* SORT */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-/**void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+/**void quick_sort(int *array, size_t size);
 **/
 #endif
