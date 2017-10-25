c68df985e1c635b416b02dcabbce56b25c1b87ce
jvpupcat <vuong.joann@yahoo.com>
2017-10-25 17:46:00 +0000

    fixed betty error

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - function that creates a binary tree node
+ * binary_tree_node - function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value is the value to put in the new node
  * Return: pointer to the new node of NULL upon failure
