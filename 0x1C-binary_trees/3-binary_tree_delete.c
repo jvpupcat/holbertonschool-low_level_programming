f9cbc01ffa0cdcc74a81185a81a02e373db1d5a8
jvpupcat <vuong.joann@yahoo.com>
2017-10-26 20:48:32 +0000

    first commit

---
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: none
+ **/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
