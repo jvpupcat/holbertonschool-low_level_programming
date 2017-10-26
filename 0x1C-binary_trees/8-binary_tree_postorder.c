e3832d32da3a17b6cef4013bd1996b1c42753570
jvpupcat <vuong.joann@yahoo.com>
2017-10-26 21:29:20 +0000

    fixed betty error

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree
- * using pre-order traversal
+ * binary_tree_postorder - function that goes through a binary tree
+ * using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node.
  * Return: nothing if tree or func is NULL
