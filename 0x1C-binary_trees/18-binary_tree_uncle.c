625b53e38271547ceb70b48efa07ec14c95ad512
jvpupcat <vuong.joann@yahoo.com>
2017-10-27 23:15:46 +0000

    fixed errors

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -9,10 +9,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (NULL);
-	if (node->parent == NULL || node->parent->parent == NULL)
+	if (node->parent == NULL)
 		return (NULL);
-	if (node->parent->right->left == node)
-		return (node->parent->parent->left);
-	else
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
