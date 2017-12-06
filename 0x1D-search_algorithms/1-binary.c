5e1925968a56a59ed719888f64ed0580654d9089
jvpupcat <vuong.joann@yahoo.com>
2017-12-06 18:00:32 +0000

    added print stmt

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,6 +1,26 @@
 #include "search_algos.h"
 
 /**
+ * print_stmt - function that prints stmt
+ * @array: array of int
+ * @left: left side of array
+ * @right: right side of array
+ * Return: None
+ **/
+void print_stmt(int *array, int left, int right)
+{
+	int i;
+	printf("Searching in array: ");
+	for (i = left; i < right + 1; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
  * binary_search - function that searches for a value in a sorted
  * array of integers using the Binary search algorithm
  * @array: array of integers
@@ -10,16 +30,17 @@
  **/
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int i, middle, left, right;
+	int middle, left, right;
 
 	left = 0;
 	right = size - 1;
 
 	if (array == NULL)
 		return (-1);
-	for (i = 0; i < size; i++)
+	while (left <= right)
 	{
 		middle = (left + right) / 2;
+		print_stmt(array, left, right);
 		if (array[middle] == value)
 			return (middle);
 		else if (array[middle] < value)
