3bb4414c66c68aca0107838afba2169c94ead388
jvpupcat <vuong.joann@yahoo.com>
2017-12-06 07:38:49 +0000

    added () to a return statement

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 	{
 		middle = (left + right) / 2;
 		if (array[middle] == value)
-			return middle;
+			return (middle);
 		else if (array[middle] < value)
 		{
 			left = middle + 1;
