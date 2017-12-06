3c88c301bfb841ab9876e58f64671eaf14b78a98
jvpupcat <vuong.joann@yahoo.com>
2017-12-06 07:36:39 +0000

    fixed for loop

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -14,23 +14,22 @@ int binary_search(int *array, size_t size, int value)
 
 	left = 0;
 	right = size - 1;
-	middle = (left + right) / 2;
 
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Searching in array: %d", array[i])
-		if (array[middle] < value)
-		{
-			left = m + 1;
-		}
-		if (array[middle] > value)
-		{
-			right = m - 1;
-		}
+		middle = (left + right) / 2;
 		if (array[middle] == value)
 			return middle;
+		else if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else /* (array[middle] > value) */
+		{
+			right = middle - 1;
+		}
 	}
 	return (-1);
 }
