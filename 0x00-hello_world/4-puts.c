1a9b00df48b35ba9282aed727e54c48ec4761f27
jvpupcat <vuong.joann@yahoo.com>
2017-12-14 12:56:53 +0000

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Write a script that compiles a C file and creates
+ * an executable named cisfun
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
