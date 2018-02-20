21b81242651fc34935cdb5f981537a1044415151
jvpupcat <130@holbertonschool.com>
2018-02-20 08:54:50 +0000

    added struct

--- warm_up/binary_trees.h
+++ warm_up/binary_trees.h
@@ -2,7 +2,31 @@
 #define BINARY_TREES
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
+/*STRUCT*/
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ **/
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+/*DEFAULT FUNCTIONS*/
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+static size_t _height(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *tree);
+
+/*BINARY TREE FUNCTIONS*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_full(const binary_tree_t *tree);
